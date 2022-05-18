a513cd9021b8ee4fa59781c6faf6c34a2f17e144
Mario Hernández <marioanhelo@hotmail.com>
2022-05-18 16:56:22 -0500

    Cambios en busqueda

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
-let discosTable = document.getElementById('tableDiscos');
-let btnSearch = document.getElementById('btn-search');
-let closebtn = document.getElementById('closebtn');
-btnSearch.addEventListener("click", searchDisc)
-closebtn.addEventListener("click",closeSearch)
-class Disco{
-    constructor(ID,Name,Price,Year){
-        this.ID = ID;
-        this.Name=Name;
-        this.Price = Price;
-        this.Year = Year
-    }
-}
-class DiscoResults{
-    constructor(ID,Name,Price,Year){
-        this.ID = ID;
-        this.Name=Name;
-        this.Price = Price;
-        this.Year = Year
-    }
-}
-
-listaDiscos = []
-listResults = []
+const discosTable = document.getElementById('tableDiscos');
+const btnSearch = document.getElementById('btn-search');
+const closebtn = document.getElementById('closebtn');
+const inputSearch = document.getElementById('search');
+const cells = document.getElementsByTagName('tr');
 const buttonRegistro = document.getElementById("registroDisco");
-buttonRegistro.addEventListener("click",getDatosDisco)
-function getDatosDisco(){
+buttonRegistro.addEventListener("click", registroDisco)
+inputSearch.addEventListener('keyup', (e) => {
+    let query = e.target.value
+    let search = new RegExp(query, "i")
+    for (let i = 0; i < cells.length; i++) {
+        let valor = cells[i]
+        if (search.test(valor.innerText)) {
+            valor.classList.remove('ocultar')
+        } else {
+            valor.classList.add('ocultar')
+        }
+    }
+})
+listaDiscos = []
+class Disco {
+    constructor(ID, Name, Price, Year) {
+        this.ID = ID;
+        this.Name = Name;
+        this.Price = Price;
+        this.Year = Year
+    }
+}
+listaDiscos.push(new Disco('1652909617207', "The Hidden Land", "300", "2019"))
+listaDiscos.push(new Disco('1666709617208', "Little Worlds", "250", "2018"))
+listaDiscos.push(new Disco('1652959617209', "Live Art", "150", "2020"))
+listaDiscos.push(new Disco('1652903617210', "Cool", "300", "2017"))
+listaDiscos.push(new Disco('1652925961721', "Tales from Acoustic", "200", "1995"))
+renderTable()
+
+
+function registroDisco() {
     let UUID = Date.now()
     let nombreDisco = document.getElementById("inputName").value
     let precioDisco = document.getElementById("inputPrice").value
     let yearDisco = document.getElementById("inputYear").value
-    const disco =new Disco(UUID,nombreDisco,precioDisco,yearDisco)
+    const disco = new Disco(UUID, nombreDisco, precioDisco, yearDisco)
     listaDiscos.push(disco)
     swal({
         title: "Success",
@@ -40,16 +50,14 @@ function getDatosDisco(){
     renderTable();
     clearInputs();
 }
-function clearInputs(){
-document.getElementById("inputName").value = ""
-document.getElementById("inputPrice").value = ""
-document.getElementById("inputYear").value= ""
+function clearInputs() {
+    document.getElementById("inputName").value = ""
+    document.getElementById("inputPrice").value = ""
+    document.getElementById("inputYear").value = ""
 }
-function print(){}
-
-function confirmDelete(IDdisco){
-	var msg = "Seguro de eliminar?";
-	swal(msg, {
+function confirmDelete(IDdisco) {
+    var msg = "Seguro de eliminar?";
+    swal(msg, {
         icon: "warning",
         buttons: {
             NO: {
@@ -61,21 +69,19 @@ function confirmDelete(IDdisco){
                 value: "SI",
             },
         },
-    }).then(function(value) {
+    }).then(function (value) {
         switch (value) {
             case "SI":
                 deleteItem(IDdisco)
-               break
-
+                break;
             case "no":
                 break;
         }
-    });
-
+    })
 }
-function deleteItem(ID){
-    for(let i = 0; i< listaDiscos.length; i++) {
-        if (listaDiscos[i].ID == ID){
+function deleteItem(ID) {
+    for (let i = 0; i < listaDiscos.length; i++) {
+        if (listaDiscos[i].ID == ID) {
             listaDiscos.splice(i, 1);
             swal({
                 title: "Success",
@@ -83,115 +89,16 @@ function deleteItem(ID){
                 timer: 3000,
                 icon: "success",
             });
-            renderTable()
-        }else{
-            swal({
-                title: "Error",
-                text: "Hubo un error",
-                icon: "error",
-            });
+            renderTable();
         }
     }
-
 }
-
-function renderTable(){
-    var DiscsTableData = document.getElementById("tableDiscos")
-    var bodyTable= document.getElementById("discosList")
+function renderTable() {
+    var bodyTable = document.getElementById("discosList")
     bodyTable.innerHTML = "";
-    for (let i =0;i<listaDiscos.length;i++){
-
-        var tbody = document.getElementById("discosList")
-        var row = document.createElement("tr")
-        var idColumn = document.createElement("td")
-        var nameColumn = document.createElement("td")
-        var priceColumn = document.createElement("td")
-        var yearColumn = document.createElement("td")
-        var btnColumn = document.createElement("td")
-
-        var Id = document.createTextNode(listaDiscos[i].ID)
-        var Nombre = document.createTextNode(listaDiscos[i].Name)
-        var Precio = document.createTextNode(`$${listaDiscos[i].Price}`)
-        var Anyo = document.createTextNode(listaDiscos[i].Year)
-        var btn = document.createElement("div")
-        btn.innerHTML = `<button type="button" class="btn btn-sm btn-light" onclick="confirmDelete('${listaDiscos[i].ID}')"><i class="fa fa-trash-o fa-2x text-danger" aria-hidden="true"></i></button>`
-
-        idColumn.appendChild(Id)
-        nameColumn.appendChild(Nombre)
-        priceColumn.appendChild(Precio)
-        yearColumn.appendChild(Anyo)
-        btnColumn.appendChild(btn)
-
-        row.appendChild(idColumn)
-        row.appendChild(nameColumn)
-        row.appendChild(priceColumn)
-        row.appendChild(yearColumn)
-        row.appendChild(btnColumn)
-        tbody.appendChild(row)
-        DiscsTableData.appendChild(tbody)
+    let = rowBody = ""
+    for (let i = 0; i < listaDiscos.length; i++) {
+        rowBody += `<tr><td>${listaDiscos[i].ID}</td><td>${listaDiscos[i].Name}</td><td>$${listaDiscos[i].Price}</td><td>${listaDiscos[i].Year}</td><td><button type="button" class="btn btn-sm btn-light" onclick="confirmDelete('${listaDiscos[i].ID}')"><i class="fa fa-trash-o fa-2x text-danger" aria-hidden="true"></i></button></td></tr>`
     }
-}
-function searchDisc(){
-
-    let query = document.getElementById("search").value
-    if(query ==""){
-        swal({
-            title: "Error",
-            text: "Escribe el nombre a buscar",
-            timer: 2000,
-            icon: "error",
-        });
-        return false
-    }
-    for(let i = 0; i< listaDiscos.length; i++) {
-        if (listaDiscos[i].Name == query){
-            let searchresult= new DiscoResults(listaDiscos[i].ID,listaDiscos[i].Name,listaDiscos[i].Price,listaDiscos[i].Year)
-            listResults.push(searchresult)
-        }
-    }
-    renderResults()
-}
-
-function renderResults(){
-    var tableResults = document.getElementById("searchResults")
-    var bodyTable= document.getElementById("dataResults")
-    bodyTable.innerHTML = "";
-    let lnt = listResults.length
-    if(lnt==0){
-        swal({
-            title: "Lo sentimos",
-            text: "Sin resultados",
-            icon: "warning",
-        });
-    }else{
-        let card = document.getElementById('searchResults');
-        card.style.display="block"
-        for (let i =0;i<listResults.length;i++){
-            var tbodyResults = document.getElementById("dataResults")
-            var row = document.createElement("tr")
-            var idColumn = document.createElement("td")
-            var nameColumn = document.createElement("td")
-            var priceColumn = document.createElement("td")
-            var yearColumn = document.createElement("td")
-            var Id = document.createTextNode(listResults[i].ID)
-            var Nombre = document.createTextNode(listResults[i].Name)
-            var Precio = document.createTextNode(`$${listResults[i].Price}`)
-            var Anyo = document.createTextNode(listResults[i].Year)
-            idColumn.appendChild(Id)
-            nameColumn.appendChild(Nombre)
-            priceColumn.appendChild(Precio)
-            yearColumn.appendChild(Anyo)
-            row.appendChild(idColumn)
-            row.appendChild(nameColumn)
-            row.appendChild(priceColumn)
-            row.appendChild(yearColumn)
-            tbodyResults.appendChild(row)
-        }
-        listResults.length = 0
-    }
-
-}
-function closeSearch(){
-    let card = document.getElementById('searchResults');
-        card.style.display="none"
+    document.getElementById("discosList").innerHTML = rowBody
 }
